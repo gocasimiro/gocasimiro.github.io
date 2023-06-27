3743a603c0ddc2c4960678d0aa97821cd52f4595
Guilherme Casimiro <gocasimiro@gmail.com>
2023-06-26 22:07:27 -0300

    first release

---
+++ magic.js
@@ -0,0 +1,40 @@
+const themeToggler = () => {
+	const dark = () => {
+		document.body.style.background = 'black'
+		document.body.style.color = 'white'
+	}
+	const light = () => {
+		document.body.style.background = 'white'
+		document.body.style.color = 'black'
+	}
+
+	if (
+		window.matchMedia &&
+		window.matchMedia('(prefers-color-scheme: dark)').matches
+	) {
+		dark()
+	} else {
+		light()
+	}
+
+	window
+		.matchMedia('(prefers-color-scheme: dark)')
+		.addEventListener('change', () => {
+			themeToggler()
+		})
+}
+
+const contactor = () => {
+	document.querySelector('h4').addEventListener('mouseenter', () => {
+		document.querySelector('ul').style.display = 'inline-block'
+	})
+}
+
+const titleChanger = () => {
+	if (window.document.readyState) document.title = 'boy, that was fast 😮‍💨'
+	setTimeout(() => (document.title = "who's gocas?"), 1850)
+}
+
+titleChanger()
+contactor()
+themeToggler()
